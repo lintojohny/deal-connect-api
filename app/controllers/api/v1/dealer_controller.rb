baf0db94ef1675f3eb9e8c29b9056bb9c75cc152
Rutul Shah <shahrutul1993@github.com>
2017-04-04 01:16:43 +0530

    fixed error

--- app/controllers/api/v1/dealer_controller.rb
+++ app/controllers/api/v1/dealer_controller.rb
@@ -2,4 +2,18 @@ class Api::V1::DealerController < ApplicationController
 	def index
 		render json: Dealer.all, status: :ok
 	end	
+
+	def create
+		@dealer_item = Dealer.new(params.require(:dealer).permit(:id , :title, :description, :vote))
+		if @dealer_item.save
+			render json: @dealer_item, status: :created
+		else
+			render json: @dealer_item.error, status: :unprocessable_entity
+		end		
+	end
+
+	def show
+		@dealer = Dealer.find(params[:id])
+		render json: @dealer
+	end
 end
